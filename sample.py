6c5633239c94ae79e7a5d0d9fee74baa8bf4b793
Kamil Strzempowicz <konserw@gmail.com>
2020-03-21 18:43:44 +0100

    Add sample.py

---
+++ sample.py
@@ -0,0 +1,14 @@
+from Qt5 import QtCore, QtWidgets
+from hamcrest import assert_that
+
+from qtmatchers import has_item_flags
+
+
+class TestSample:
+    def test_pass(self):
+        item = QtWidgets.QListWidgetItem()
+        assert_that(item.flags(), has_item_flags(QtCore.Qt.ItemIsEnabled | QtCore.Qt.ItemIsDragEnabled | QtCore.Qt.ItemIsUserCheckable | QtCore.Qt.ItemIsSelectable))
+
+    def test_fail(self):
+        item = QtWidgets.QListWidgetItem()
+        assert_that(item.flags(), has_item_flags(QtCore.Qt.ItemIsEnabled))
